454f937dbef927f0071158fb606cbc383f9cadbd
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-08-25 19:24:41 +0500

    create

---
+++ AIC batch 37 Assignment/Quarter 1/Find Even Odd number Assignment 2 class 3.py
@@ -0,0 +1,15 @@
+
+"""
+Created on Thu Aug 25 19:07:44 2022
+
+@author: samiwadho
+"""
+
+#               Assignment 2 class 3
+# Finding even odd number by putting number by user
+number =int(input('Enter the Number = '))
+#Even = 0,2,4,6,8,10 and Odd =1,3,5,7,9,11
+if number == 0:
+    print('The number is Odd')
+else :
+    print('The number is Even')
