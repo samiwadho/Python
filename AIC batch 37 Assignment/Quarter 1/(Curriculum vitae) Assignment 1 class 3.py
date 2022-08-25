dd7afebb0e197ec6903fb9b64bcddb46c0b0e8bc
samiwadho <96658347+samiwadho@users.noreply.github.com>
2022-08-25 19:23:58 +0500

    create

---
+++ AIC batch 37 Assignment/Quarter 1/(Curriculum vitae) Assignment 1 class 3.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  3 21:29:04 2022
+
+@author: samiwadho
+"""
+
+
+
+print(              '\n\t\t\t\tCurriculum Vitae')
+print('\n\nAbdul Sami Wadho')
+print('samiwadho@gmail.com')
+print('+923163476700')
+print("\n\nObjected")
+print('A highly Motivated Student from University of Sindh,Aim to use my Knowledge as')
+print("interne inyou firm, to remain updae in my field")
+
+
+print('\n\t\t\t\t\t\tAcademic Qualification')
+print("\n|________|_____________________________________|________________|____________________|____________|________|")
+print("| DEGREE\t| Name of University/College/School \t| Board\t\t\t|  Group\t\t\t\t| Year\t\t| C.G.P.C|")
+print("|________|_____________________________________|________________|____________________|____________|________|")
+print("|B.Sc\t|University of Sindh\t\t\t\t\t|Jamshoro\t\t|Computer Science\t\t| Continue\t|...",'    |')
+print("|H.S.C\t|GOVT.Shah Latif Degree College Kamber\t|Larkana B.I.S.E\t|Pre-engineering\t\t| 2019\t\t| 81%",'   |')
+print("|S.S.C\t|GOVT.Boys High School Kamber\t\t\t|Larkana B.I.S.E \t|Science \t\t\t|2017\t\t| 78% ",'  |')
+print('|________|_____________________________________|________________|____________________|____________|________|')
+
+print('\n\t\t\t\t\t\tLanguage Skill')
+print('Sindhi Fluent')
+print('Urdu Fluent')
+print('English Fluent')
+
+print('\n\t\t\t\t\t\tAttended Seminar')
+print('\nUniversity Of Sindh Jamshoro Welcome 2k20  Seminar')
+print('GDSC GIT&GITHUB events')
+print('Oracle workshop seminar')
+print('Web development workshop seminar ')
+print('Volunteer for PIAIC entry test of 36&37 batch at Karachi stadium')
+
+print('\n\t\t\t\t\t\tPersonal information')
+print('•	Name 			  :	Abdul Sami')
+print('•	Father’s Name	  :	Abdullah')
+print('•	Caste			  :	Wadho')
+print('•	Date of birth	  :	01/01/2002')
+print('•	Gender 		      :	Male')
+print('•	Religion		      :	Islam')
+print('•	Nationality 	      :	Pakistani')
+
+print('\n\t\t\t\t\t\tHobbies')
+print('•	Search New Technogies ')
+print('•	Coding')
+print('•	Reading Blog ')
+print('•	Reading Book ')
+
+print('\n\t\t\t\t\t\tFavourite Books')
+print('•	 “The Alchemist” by Paulo Coelho')
+print('•	 “The last lecture”')
+print('•	 “Nuskha haya wafa” by faiz ahmed faiz')
+
+
+
+
+
